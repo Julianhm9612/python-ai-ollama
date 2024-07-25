6a0edbad7152e75005238f40a29c6b489bba1c67
Julian Henao Marin <julianhm9612@gmail.com>
2024-07-25 12:49:58 -0500

    Add initial code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import ollama
 
-response = ollama.chat(model='llama2', messages=[
+response = ollama.chat(model='llama3', messages=[
   {
     'role': 'user',
     'content': 'Why is the sky blue?',
